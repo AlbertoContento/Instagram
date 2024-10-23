5c2884508a36b03d3e87639fa440b6a60456cdeb
Alberto Contento <albertillo_ec@hotmail.com>
2024-10-23 13:52:01 +0200

    Create post in admin.py and create model comment and Post

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Post(models.Model):
+    user = models.ForeignKey(User, verbose_name="Usuario", on_delete=models.CASCADE, related_name='posts')
+    image = models.ImageField("Imagen", upload_to='posts/')
+    caption = models.TextField("Comentario", max_length=500, blank=True)
+    created_at = models.DateTimeField("Fecha de Creacion", auto_now_add=True)
+    #ESte campo es para saber que usuarios le dan a me gusta a este post
+    likes = models.ManyToManyField(User, verbose_name="Me gusta", related_name='liked_posts')
+    
+    class Meta:#Para decir como quiero que me aparezca en el Admin
+        verbose_name = 'Post'
+        verbose_name_plural = 'Posts'
+
+    def __str__(self):
+        return f"{self.user.username} - {self.created_at}"
+    
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    user = models.ForeignKey(User, verbose_name="Usuario", on_delete=models.CASCADE, related_name='comments')
+    text = models.TextField("Texto", max_length=300)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'Comentario'
+        verbose_name_plural = 'Comentarios'
+
+    def __str__(self):
+        return f"Coméntó {self.user.username} el post {self.post}"
--- profiles/models.py
+++ profiles/models.py
@@ -3,11 +3,12 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class UserProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    user = models.OneToOneField(User, verbose_name= "Usuario", on_delete=models.CASCADE, related_name='profile')
     profile_picture = models.ImageField('Imagen de perfil', upload_to='profile_pictures/', blank=True, null=True)
     bio = models.TextField('Biografía', max_length=500, blank=True)
     birth_date = models.DateField('Fecha de nacimiento', null=True, blank=True)
-    #followers = models.ManyToManyField("self", symmetrical=False, related_name='following', through='Follow')
+    #Este campo dice que no es simetrico(si un user sigue a otro el otro no tiene porque seguirle), through es para ver si queremos tener un campo intermedio para saber quien sigue a quien y desde cuando
+    followers = models.ManyToManyField("self", symmetrical=False, related_name='following', through='Follow')
 
     class Meta:#Para decir como quiero que me aparezca en el Admin
         verbose_name = 'Perfil'
@@ -15,3 +16,21 @@ class UserProfile(models.Model):
 
     def __str__(self):
         return self.username
+    
+#Creamos el modelo de follow para poder ver desde cuando sigue a alguien y a quien sigue y quien le sigue
+class Follow(models.Model):
+    follower = models.ForeignKey(UserProfile, verbose_name='¿Quien sigue?', on_delete=models.CASCADE, related_name='follower_set')
+    following = models.ForeignKey(UserProfile, verbose_name='¿A quien sigue?', on_delete=models.CASCADE, related_name='following_set')
+    #para poder guardar en que fecha comenzo el seguimiento
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='¿Desde cuando lo sigue?')
+
+#Con esto le decimos que un user no puede seguir al mismo tipo de perfil mas de una vez
+    class Meta:
+        unique_together = ('follower', 'following')
+
+    def __str__(self):
+        return f"{self.follower} follows {self.following}"
+    
+    class Meta:
+        verbose_name = 'Seguidor'
+        verbose_name_plural = 'Seguidores'
